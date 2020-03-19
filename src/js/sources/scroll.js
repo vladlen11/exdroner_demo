f2e489d129a5c208676e99ea2d10d1b7ac9261a6
Владен <vladlen11.89@rambler.ru>
2020-03-19 09:26:02 +0200

    send

--- src/js/sources/scroll.js
+++ src/js/sources/scroll.js
@@ -1,46 +1,72 @@
 $(document).ready(function() {
+    if ($(".home").length >= 1) {
+        const $window = $(window);
+        const $document = $(document);
+        let $header = $('.header');
+        let $btnMenuHeader = $(".btn-header-menu");
+        let $btnMenuHero = $(".hero__menu .btm-menu");
+        // let $sectionHeight = $section[i].offsetTop;
+        let $fixed1 = false;
+        let $showClass = 'fixed';
 
-    const $window = $(window);
-    let $section = $("section");
-    let $btnMenu = $(".header-item");
-    let $sectionNumber = 0;
-    // let $sectionHeight = $section[i].offsetTop;
+        console.log($btnMenuHeader);
+        console.log($btnMenuHeader[0]);
+        console.log($btnMenuHero[1]);
 
-    // console.log($section , 'section');
-    // console.log($btnMenu.eq(1).addClass('test') , '$btnMenu');
-    // console.log($btnMenu[0].addClass('sadf') , '$btnMenu');
-    // console.log($section.outerHeight() , '$section[0].outerHeight()');
+        function showFixedMenu(fixed, index) {
 
-    //
-    for (let i=0; i < $section.length; i++ ) {
-        // $sectionHeight = $section[i].offsetTop;
+            if(fixed) {
+                $btnMenuHeader.eq( index ).toggleClass('fixed');
+                $btnMenuHero.eq( index ).toggleClass('show');
+            } else {
+                $btnMenuHeader.eq( index ).toggleClass('fixed');
+                $btnMenuHero.eq( index ).toggleClass('show');
+            }
+        }
+
+        $window.scroll(function(){
+            const height = $window.scrollTop();
+            const height2 = $document.scrollTop();
+            console.log(height, 'height');
+            console.log(height2, 'height2');
+            // if(height >= $btnMenuHero[0].offsetTop ) {
+            //      showFixedMenu(true, 0);
+            // } else {
+            //      showFixedMenu(false, 0);
+            // }
+            // if(height >= $btnMenuHero[1].offsetTop ) {
+            //     showFixedMenu(true, 1);
+            // } else {
+            //     showFixedMenu(false, 1);
+            // }
+            // if(height >= $btnMenuHero[2].offsetTop ) {
+            //     showFixedMenu(true, 2);
+            // } else {
+            //     showFixedMenu(false, 2);
+            // }
+
+        });
+
+        3
+        4
+        5
+        6
+        7
+        8
+        9
+        10
+        11
+
+        $('.flowing-scroll').on( 'click', function(){
+            var el = $(this);
+            var dest = el.attr('href'); // получаем направление
+            if(dest !== undefined && dest !== '') { // проверяем существование
+                $('html').animate({
+                        scrollTop: $(dest).offset().top // прокручиваем страницу к требуемому элементу
+                    }, 500 // скорость прокрутки
+                );
+            }
+            return false;
+        });
     }
-    console.log($section.eq(0).outerHeight(), ' $section.eq(0).outerHeight() ');
-    console.log($section[0].offsetTop, ' $section.eq(0).offsetTop ');
-    $window.scroll(function(){
-        const height = $window.scrollTop();
-
-        if(height >= $section[0].offsetTop + ( $section.eq(0).outerHeight() / 4 ) ) {
-            $btnMenu.eq(0).addClass('fixed');
-        } else {
-            $btnMenu.eq(0).removeClass('fixed');
-        }
-
-        if(height >= $section[1].offsetTop + ( $section.eq(1).outerHeight() / 4 ) ) {
-            $btnMenu.eq(1).addClass('fixed');
-        } else {
-            $btnMenu.eq(1).removeClass('fixed');
-        }
-
-        if(height >= $section[2].offsetTop + ( $section.eq(2).outerHeight() / 4 ) ) {
-            $btnMenu.eq(2).addClass('fixed');
-        } else {
-            $btnMenu.eq(2).removeClass('fixed');
-        }
-
-
-    });
-
-
-
 });

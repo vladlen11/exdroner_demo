60537ac6f825a8893f0fda760928d5b528d30497
Владен <vladlen11.89@rambler.ru>
2020-03-21 16:33:16 +0200

    details

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,11 @@
 //= sources/jquery.js
-//= sources/action.js
 //= sources/bootstrap.js
-//= sources/slick.js
-//= sources/slick-activate.js
-//= sources/menuHover.js
-//= sources/menuLinkClick.js
 //= sources/scroll.js
+//= sources/jquery.magnific-popup.min.js
 
+$(document).ready(function() {
+    $('.popup-link').magnificPopup({
+        type: 'image'
+        // other options
+    });
+});

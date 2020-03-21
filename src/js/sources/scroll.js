a27f6c8acbc31cb0e64f2d55e080c729c3fae73b
Владен <vladlen11.89@rambler.ru>
2020-03-21 14:36:24 +0200

    home

--- src/js/sources/scroll.js
+++ src/js/sources/scroll.js
@@ -1,65 +1,14 @@
 $(document).ready(function() {
     if ($(".home").length >= 1) {
         const $window = $(window);
-        const $document = $(document);
-        let $header = $('.header');
-        let $btnMenuHeader = $(".btn-header-menu");
-        let $btnMenuHero = $(".hero__menu .btm-menu");
-        // let $sectionHeight = $section[i].offsetTop;
-        let $fixed1 = false;
-        let $showClass = 'fixed';
+        let $section = $("section");
+        let $btnHero = $(".btn-menu");
+        let $btnHeader = $(".header-nav");
 
-        console.log($btnMenuHeader);
-        console.log($btnMenuHeader[0]);
-        console.log($btnMenuHero[1]);
-
-        function showFixedMenu(fixed, index) {
-
-            if(fixed) {
-                $btnMenuHeader.eq( index ).toggleClass('fixed');
-                $btnMenuHero.eq( index ).toggleClass('show');
-            } else {
-                $btnMenuHeader.eq( index ).toggleClass('fixed');
-                $btnMenuHero.eq( index ).toggleClass('show');
-            }
-        }
-
-        $window.scroll(function(){
-            const height = $window.scrollTop();
-            const height2 = $document.scrollTop();
-            console.log(height, 'height');
-            console.log(height2, 'height2');
-            // if(height >= $btnMenuHero[0].offsetTop ) {
-            //      showFixedMenu(true, 0);
-            // } else {
-            //      showFixedMenu(false, 0);
-            // }
-            // if(height >= $btnMenuHero[1].offsetTop ) {
-            //     showFixedMenu(true, 1);
-            // } else {
-            //     showFixedMenu(false, 1);
-            // }
-            // if(height >= $btnMenuHero[2].offsetTop ) {
-            //     showFixedMenu(true, 2);
-            // } else {
-            //     showFixedMenu(false, 2);
-            // }
-
-        });
-
-        3
-        4
-        5
-        6
-        7
-        8
-        9
-        10
-        11
 
         $('.flowing-scroll').on( 'click', function(){
-            var el = $(this);
-            var dest = el.attr('href'); // получаем направление
+            let el = $(this);
+            let dest = el.attr('href'); // получаем направление
             if(dest !== undefined && dest !== '') { // проверяем существование
                 $('html').animate({
                         scrollTop: $(dest).offset().top // прокручиваем страницу к требуемому элементу
@@ -68,5 +17,33 @@ $(document).ready(function() {
             }
             return false;
         });
+
+        $window.scroll(function(){
+            const $height = $window.scrollTop();
+
+            if($height >= $btnHero.eq(0).offset().top - 50) {
+                $btnHeader.eq(0).addClass('fixed');
+                $btnHero.eq(0).removeClass('show');
+
+            } else {
+                $btnHeader.eq(0).removeClass('fixed');
+                $btnHero.eq(0).addClass('show');
+            }
+            if($height >= $btnHero.eq(1).offset().top - 50) {
+                $btnHeader.eq(1).addClass('fixed');
+                $btnHero.eq(1).removeClass('show');
+            } else {
+                $btnHeader.eq(1).removeClass('fixed');
+                $btnHero.eq(1).addClass('show');
+            }
+            if($height >= $btnHero.eq(2).offset().top - 50) {
+                $btnHeader.eq(2).addClass('fixed');
+                $btnHero.eq(2).removeClass('show');
+            } else {
+                $btnHeader.eq(2).removeClass('fixed');
+                $btnHero.eq(2).addClass('show');
+            }
+        });
     }
+
 });
